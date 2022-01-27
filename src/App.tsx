3efe760ebb92fe791ef2d7b633930ee3c8eda408
fkingBug <fkingBug@mail.com>
2022-01-27 19:30:03 +0300

    bata228

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  return <div className='App'>Я тут батя</div>
+  return <div className='App'>Я тут батя228</div>
 }
 
 export default App
