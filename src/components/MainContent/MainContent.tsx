fa1cb217413bb9fcbded48d05f01e7b658c24738
misha <i.misha.kam@gmail.com>
2022-02-01 22:57:08 +0300

    fix conf

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Box } from '@material-ui/core';
-import { AppRouter } from '../AppRouter/AppRouter';
+import { AppRouter } from '../AppRouter';
 import { useClasses } from './MainContent.style';
 
 export const MainContent: FC = () => {
