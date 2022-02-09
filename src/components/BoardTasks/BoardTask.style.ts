352fabd44a3b05f530dccb51f687d972873262a7
misha <i.misha.kam@gmail.com>
2022-02-09 22:56:11 +0300

    wait dimas

--- src/components/BoardTasks/BoardTask.style.ts
+++ src/components/BoardTasks/BoardTask.style.ts
@@ -1,12 +1,15 @@
 import { makeStyles } from '@material-ui/core';
 
-export const useClasses = makeStyles(() => ({
+export const useClasses = makeStyles((theme) => ({
   container: {
     width: '100%',
     height: '100%',
     display: 'flex',
+    flexFlow: 'row reverse',
   },
   element: {
     width: '20%',
+    border: '1px solid black',
+    height: theme.spacing(10),
   },
 }));
